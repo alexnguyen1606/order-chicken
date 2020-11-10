6bcbec7931ce4bb86949f408c66c1eac712d43de
binhdq <binhdao96@gmail.com>
2020-11-10 09:13:49 +0700

    update service

--- src/main/java/com/order/service/DishCategoryService.java
+++ src/main/java/com/order/service/DishCategoryService.java
@@ -1,11 +1,16 @@
 package com.order.service;
 
+import com.order.entities.DishCategory;
 import com.order.repository.DishCategoryRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
-public class DishCategoryService {
-    private DishCategoryRepository dishCategoryRepository;
+public class DishCategoryService extends CommonRepository<DishCategory,DishCategoryRepository> {
+
+
+    public DishCategoryService(DishCategoryRepository repo) {
+        super(repo);
+    }
 }
--- src/main/java/com/order/service/DishService.java
+++ src/main/java/com/order/service/DishService.java
@@ -1,11 +1,15 @@
 package com.order.service;
 
+import com.order.entities.Dish;
 import com.order.repository.DishRepository;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-@AllArgsConstructor
-public class DishService {
-    private DishRepository dishRepository;
+public class DishService extends CommonRepository<Dish, DishRepository> {
+   
+
+    public DishService(DishRepository repo) {
+        super(repo);
+    }
 }
