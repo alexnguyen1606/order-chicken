11f736f4076003097be6c981e2cf65dab7836ad6
binhdq <binhdao96@gmail.com>
2020-11-10 10:49:13 +0700

    update repo

--- src/main/java/com/order/processor/DishProcessor.java
+++ src/main/java/com/order/processor/DishProcessor.java
@@ -1,8 +1,15 @@
 package com.order.processor;
 
+import com.order.service.DishCategoryService;
+import com.order.service.DishService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DishProcessor {
+    @Autowired
+    private DishService dishService;
+    @Autowired
+    private DishCategoryService dishCategoryService;
 }
