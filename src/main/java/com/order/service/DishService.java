3840f6ca665a63af04b01d360f7d461bea2214f0
alexnguyen1606 <natuan1606@gmail.com>
2020-11-27 09:40:07 +0700

    update

--- src/main/java/com/order/service/DishService.java
+++ src/main/java/com/order/service/DishService.java
@@ -4,6 +4,7 @@ import com.order.entities.Dish;
 import com.order.entities.QDish;
 import com.order.repository.DishRepository;
 import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAUpdateClause;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -23,5 +24,16 @@ public class DishService extends CommonRepository<Dish, DishRepository> {
 
   public List<Dish> findByCategoryAndStatus(Long categoryId, String status) {
     return repo.findByIdCategoryAndStatus(categoryId, status);
+  }
+  
+  public void updateStatus(Long id,String status){
+    JPAUpdateClause update = new JPAUpdateClause(em,Q);
+    update.set(Q.status,status);
+    update.where(Q.id.eq(id)).execute();
+  }
+  public void updateAllStatus(List<Long> ids,String status){
+    JPAUpdateClause update = new JPAUpdateClause(em,Q);
+    update.set(Q.status,status);
+    update.where(Q.id.in(ids)).execute();
   }
 }
